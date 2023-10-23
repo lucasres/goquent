59a30233a7a9775809befb60abcc0e1a42770ae5
lucasres <lucasresone@gmail.com>
2023-10-23 09:56:13 -0300

    add set test and fix erro return wrong

--- set.go
+++ set.go
@@ -14,14 +14,14 @@ type SetClause struct {
 }
 
 func (s *SetClause) ToSQL() (string, []interface{}, error) {
-	if len(s.sets) != 2 {
-		return "", nil, errors.New("invalid args for S need string, interface{}")
-	}
-
 	sql := make([]string, 0)
 	args := make([]interface{}, 0)
 
 	for _, v := range s.sets {
+		if len(v) != 2 {
+			return "", nil, errors.New("invalid args for S need string, interface{}")
+		}
+
 		column, ok := v[0].(string)
 		if !ok {
 			return "", nil, errors.New("invalid args the index 0 in S need string")
